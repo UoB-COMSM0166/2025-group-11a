e7b816690b27f039d707145246b0588f1ed39494
jackskrparrow <3098124923@qq.com>
2025-02-27 23:03:53 +0000

    Update obstacle.js

--- SnakeGame/obstacle.js
+++ SnakeGame/obstacle.js
@@ -1,55 +1,28 @@
 class ObstacleManager {
-  constructor() {
+    constructor() {
       this.obstacles = [];
-  }
-
-  generateObstacle(count = 1) {
+    }
+  
+    generateObstacle(count = 1) {
       for (let i = 0; i < count; i++) {
-          let isHorizontal = random() > 0.4;
-          let x = random((-width * 1) + 4*gridSize, (width * 1) - 4*gridSize);
-          let y = random((-height * 1) + 4*gridSize, (height * 1) - 4*gridSize);
-          let length = floor(random(4, 10)) * gridSize;
-          
-          // 检查新障碍物是否与已有障碍物重叠
-          let newObstacle = { x, y, length, isHorizontal };
-          if (!this.isOverlapping(newObstacle)) {
-              this.obstacles.push(newObstacle);
-          }
+        this.obstacles.push(createVector(
+            random(-width * 1, width * 1),
+            random(-height * 1, height * 1)
+        ));
       }
-  }
-
-  // 检查是否有重叠
-  isOverlapping(newObstacle) {
-      for (let o of this.obstacles) {
-          if (newObstacle.isHorizontal === o.isHorizontal) {
-              if (newObstacle.isHorizontal) {
-                  // 检查横向障碍物重叠
-                  if (newObstacle.y === o.y && 
-                      (newObstacle.x < o.x + o.length && newObstacle.x + newObstacle.length > o.x)) {
-                      return true;
-                  }
-              } else {
-                  // 检查竖向障碍物重叠
-                  if (newObstacle.x === o.x && 
-                      (newObstacle.y < o.y + o.length && newObstacle.y + newObstacle.length > o.y)) {
-                      return true;
-                  }
-              }
-          }
-      }
-      return false;
-  }
-
-  drawObstacles() {
+    }
+  
+    drawObstacles() {
       push();
       fill(255, 0, 0);
       for (let o of this.obstacles) {
-          if (o.isHorizontal) {
-              rect(o.x, o.y, o.length, 0.5*gridSize);
-          } else {
-              rect(o.x, o.y, 0.5*gridSize, o.length);
-          }
+        triangle(
+          o.x - gridSize * 0.4, o.y + gridSize * 0.4,  // 左下角
+          o.x + gridSize * 0.4, o.y + gridSize * 0.4,  // 右下角
+          o.x, o.y - gridSize * 0.4                    // 顶点
+        );
       }
       pop();
+    }
   }
-}
+  
